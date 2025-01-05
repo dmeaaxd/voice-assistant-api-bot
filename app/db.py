d202ab1ced5a8edde8b684461a4a88205aec6848
Maksim Ushakov <dmeaaxd@gmail.com>
2025-01-06 02:31:40 +0300

    fix db connect

--- app/db.py
+++ app/db.py
@@ -15,7 +15,7 @@ DATABASE_URL = (
 )
 
 # Создание engine и сессии
-engine = create_engine(DATABASE_URL, isolation_level="REPEATABLE READ")
+engine = create_engine(DATABASE_URL, isolation_level="READ COMMITTED")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
 Base = declarative_base()
 
